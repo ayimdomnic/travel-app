d712e60a83bfabe249645f1eab1edf87fab5213e
ayimdomnic <ayimdomnic@outlook.com>
2016-03-13 12:45:25 +0300

    to do: frontend

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
 		'mysql' => [
                     'driver'    => 'mysql',
                     'host'      => 'localhost',
-                    'database'  => 'bus_track',
+                    'database'  => 'homestead',
                     'username'  => 'root',
                     'password'  => '',
                     'charset'   => 'utf8',
